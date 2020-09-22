d305655984a5d3a34114ecc1f38ceb04b35c1d06
Michael Etim <michaelmunchin89@gmail.com>
2020-09-22 09:42:39 +0100

    Added Create Roles

---
+++ BlossomProduct/BlossomProduct/Controllers/AdministrationController.cs
@@ -0,0 +1,51 @@
+﻿using BlossomProduct.Core.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BlossomProduct.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdministrationController( RoleManager<IdentityRole> roleManager )
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole( )
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole( CreateRoleVm model )
+        {
+            if(ModelState.IsValid)
+            {
+                // We just need to specify a unique role name to create a new role
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName
+                };
+
+                // Saves the role in the underlying AspNetRoles table
+                IdentityResult result = await _roleManager.CreateAsync( identityRole );
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction( "Index", "Home" );
+                }
+
+                foreach(IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError( "", error.Description );
+                }
+            }
+
+            return View( model );
+        }
+    }
+}
