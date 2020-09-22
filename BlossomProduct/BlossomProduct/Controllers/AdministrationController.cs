f1ebc99e3c40ae570c42349ddc124f92031244f7
Michael Etim <michaelmunchin89@gmail.com>
2020-09-22 14:13:14 +0100

    Added edit roles

--- BlossomProduct/BlossomProduct/Controllers/AdministrationController.cs
+++ BlossomProduct/BlossomProduct/Controllers/AdministrationController.cs
@@ -1,4 +1,5 @@
-﻿using BlossomProduct.Core.ViewModels;
+﻿using BlossomProduct.Core.Models;
+using BlossomProduct.Core.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -8,10 +9,13 @@ namespace BlossomProduct.Controllers
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public AdministrationController( RoleManager<IdentityRole> roleManager )
+        public AdministrationController( RoleManager<IdentityRole> roleManager,
+                                        UserManager<ApplicationUser> userManager )
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -53,6 +57,72 @@ namespace BlossomProduct.Controllers
         {
             var roles = _roleManager.Roles;
             return View( roles );
+        }
+
+        // Role ID is passed from the URL to the action
+        [HttpGet]
+        public async Task<IActionResult> EditRole( string id )
+        {
+            // Find the role by Role ID
+            var role = await _roleManager.FindByIdAsync( id );
+
+            if(role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+                return View( "NotFound" );
+            }
+
+            var model = new EditRoleVm
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+
+            // Retrieve all the Users
+            foreach(var user in _userManager.Users)
+            {
+                // If the user is in this role, add the username to
+                // Users property of EditRoleViewModel. This model
+                // object is then passed to the view for display
+                if(await _userManager.IsInRoleAsync( user, role.Name ))
+                {
+                    model.Users.Add( user.UserName );
+                }
+            }
+
+            return View( model );
+        }
+
+        // This action responds to HttpPost and receives EditRoleViewModel
+        [HttpPost]
+        public async Task<IActionResult> EditRole( EditRoleVm model )
+        {
+            var role = await _roleManager.FindByIdAsync( model.Id );
+
+            if(role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {model.Id} cannot be found";
+                return View( "NotFound" );
+            }
+            else
+            {
+                role.Name = model.RoleName;
+
+                // Update the Role using UpdateAsync
+                var result = await _roleManager.UpdateAsync( role );
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction( "ListRoles" );
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError( "", error.Description );
+                }
+
+                return View( model );
+            }
         }
     }
 }
