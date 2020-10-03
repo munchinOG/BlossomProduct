2716e1b1d1956ebde8ae5013ccf04d371d861925
Michael Etim <michaelmunchin89@gmail.com>
2020-10-03 21:04:56 +0100

    List all users

--- BlossomProduct/BlossomProduct/Controllers/AccountController.cs
+++ BlossomProduct/BlossomProduct/Controllers/AccountController.cs
@@ -56,6 +56,11 @@ namespace BlossomProduct.Controllers
                 // SignInManager and redirect to index action of HomeController
                 if(result.Succeeded)
                 {
+                    if(_signInManager.IsSignedIn( User ) && User.IsInRole( "Admin" ))
+                    {
+                        return RedirectToAction( "ListUsers", "Administration" );
+                    }
+
                     await _signInManager.SignInAsync( user, isPersistent: false );
                     return RedirectToAction( "Index", "Home" );
                 }
