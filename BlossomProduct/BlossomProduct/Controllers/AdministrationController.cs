fbcfbcd37c06fc406b3cd050df057a773efad4be
Michael Etim <michaelmunchin89@gmail.com>
2020-10-05 17:23:10 +0100

    Added delete identity user

--- BlossomProduct/BlossomProduct/Controllers/AdministrationController.cs
+++ BlossomProduct/BlossomProduct/Controllers/AdministrationController.cs
@@ -23,6 +23,34 @@ namespace BlossomProduct.Controllers
             _userManager = userManager;
         }
 
+        [HttpPost]
+        public async Task<IActionResult> DeleteUser( string id )
+        {
+            var user = await _userManager.FindByIdAsync( id );
+
+            if(user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {id} cannot be found";
+                return View( "NotFound" );
+            }
+            else
+            {
+                var result = await _userManager.DeleteAsync( user );
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction( "ListUsers" );
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError( "", error.Description );
+                }
+
+                return View( "ListUsers" );
+            }
+        }
+
         [HttpGet]
         public IActionResult ListUsers( )
         {
